0764a8e7f7e7d572624dde5b9abb25d18c6b3c34
redragerover <105608997+redragerover@users.noreply.github.com>
2023-09-09 21:22:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentPage = 1;
-const itemsPerPage = 12; // Adjust as needed
+const itemsPerPage = 12; 
 
 async function fetchImages() {
     const response = await fetch('images.json');
@@ -21,14 +21,16 @@ function renderGallery(images, page) {
 }
 
 function updatePagination(images, page) {
+    const totalPages = Math.ceil(images.length / itemsPerPage);
+    
     const pageInfo = document.getElementById('page-info');
-    pageInfo.textContent = `Page ${page}`;
+    pageInfo.textContent = `Page ${page} of ${totalPages}`;
     
     const prevButton = document.getElementById('prev');
     const nextButton = document.getElementById('next');
     
     prevButton.disabled = page === 1;
-    nextButton.disabled = page * itemsPerPage >= images.length;
+    nextButton.disabled = page >= totalPages;
 
     prevButton.onclick = () => {
         currentPage = Math.max(1, page - 1);
@@ -37,9 +39,21 @@ function updatePagination(images, page) {
     };
 
     nextButton.onclick = () => {
-        currentPage = Math.min(Math.ceil(images.length / itemsPerPage), page + 1);
+        currentPage = Math.min(totalPages, page + 1);
         renderGallery(images, currentPage);
         updatePagination(images, currentPage);
+    };
+    
+    const pageInput = document.getElementById('page-input');
+    pageInput.onchange = () => {
+        const inputPage = parseInt(pageInput.value);
+        if (inputPage >= 1 && inputPage <= totalPages) {
+            currentPage = inputPage;
+            renderGallery(images, currentPage);
+            updatePagination(images, currentPage);
+        } else {
+            alert('Invalid page number');
+        }
     };
 }
 
